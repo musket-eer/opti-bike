1336969feb9760fadd16d305306dcf9c098b8d34
Jerry Onyango <jerryonyango@Jerrys-MacBook-Air-4.local>
2024-04-13 16:04:46 -0700

    added basic classes

--- artefacts/bike.py
+++ artefacts/bike.py
@@ -1,40 +1,33 @@
 import hashlib
 
 class Bike:
-    def __init__(self, bike_id, manual_lock_code):
+    def __init__(self, bike_id, manual_lock_code, rack):
         self.bike_id = bike_id
         self.manual_lock_code = manual_lock_code
-        self.user_email_map = {}  # Maps user email to bike ID
+        self.user = None
         self.needs_repair = False
+        self.rack = rack # type of rack object
+        self.is_locked = False
 
-    def assign_user(self, user_email):
-        # Hash the bike ID and user email for privacy
-        hashed_bike_id = hashlib.sha256(self.bike_id.encode()).hexdigest()
-        hashed_user_email = hashlib.sha256(user_email.encode()).hexdigest()
-        self.user_email_map[hashed_bike_id] = hashed_user_email
-
-    def rack(self):
-        # Return a list of bike rack locations (dummy data for demonstration)
-        bike_rack_locations = [
-            {"latitude": 37.7749, "longitude": -122.4194},  # Example location 1
-            {"latitude": 40.7128, "longitude": -74.0060}   # Example location 2
-            # Add more locations as needed
-        ]
-        return bike_rack_locations
+    def assign_user(self, user):
+        # assign user to this bike
+        self.user = user
 
     def lock(self):
-        # Generate a new manual lock code for the bike
-        new_lock_code = self.generate_lock_code()
-        # Send the new lock code to the user (not implemented)
-        return new_lock_code
+        self.lock  =True
 
     def repair(self):
-        # Set the bike to needs repair state
         self.needs_repair = True
 
     def generate_lock_code(self):
         # Dummy implementation to generate a new lock code
         return "1234"  # Replace with actual implementation
+
+    def update_rack(self, new_rack):
+        self.rack = new_rack
+
+        
+
 
 # Example usage:
 bike1 = Bike("ABC123", "5678")
---
+++ artefacts/map.py
@@ -0,0 +1,3 @@
+# wraps google maps API.
+# is a subset of locations in google maps
+# we can use buildings as nodes and distances as edges
