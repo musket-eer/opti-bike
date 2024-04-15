2168b92b6869e7b5bb95250d4c04e6697c7097a9
Russel Otieno <russel.docs@gmail.com>
2024-04-15 11:14:07 -0700

    update to readme, and changes to core.views

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # opti-bike
 An attempt to optimize bike usage on campus
+
+
+# installation
+
+1. Clone this repository: git clone + url
+2. Activate the virtual environment: source virtualenv/bin/activate
+3. Install necessary dependencies: pip3 install -r requirements.txt
+4. Start the development server: - cd optibike
+                                 - python3 manage.py runserver
+
--- optibike/core/views.py
+++ optibike/core/views.py
@@ -49,21 +49,6 @@ def bike_options(request):
     return render(request, 'options.html', {'bike_options': bike_options})
 
 
-#     if request.method == 'POST':
-#         bike_id = request.POST.get('bike_id')
-#         # Here you would typically handle the bike booking logic
-#         return redirect('confirmation_page', bike_id=bike_id)
-
-#     else:  # GET request
-#         user_location = request.session.get('user_location', default_location)
-#         bikes = bike.objects.filter(is_available=True)
-#         # Placeholder for sorting logic - you would implement actual distance calculation here
-#         bikes = sorted(bikes, key=lambda x: x.distance_to(user_location))
-#         return render(request, 'core/options.html', {'bikes': bikes})
-
-# def calculate_distance(bike_location, user_location):
-#     # Dummy function to calculate distances
-#     return abs(bike_location - user_location)
 
 def your_bike(request):
     return HttpResponse("here is your bike")
